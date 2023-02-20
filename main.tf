2b80e716ebab4d5ff632fa54f70648e7d296dcf9
robgutsopedra <roberto.gutierrez@ultimate.ai>
2023-02-20 15:12:26 +0100

    test

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "mongodbatlas_database_user" "test" {
     for_each = var.databases_names
     content {
       role_name     = "readWrite"
-      database_name = roles.value["databases_names"]
+      database_name = roles.value
     }
     
   }
