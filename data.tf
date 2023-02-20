42c2a0abedd43cd78c6ca48003d6910e156f1d8e
robgutsopedra <roberto.gutierrez@ultimate.ai>
2023-02-20 14:29:23 +0100

    Create user - 1 try

--- data.tf
+++ data.tf
@@ -1,11 +1,1 @@
-data "google_secret_manager_secret_version" "atlas_private_key" {
-  provider = google-beta
-  project  = "ultimateai-terraform-admin"
-  secret   = "atlas_private_key"
-}
 
-data "google_secret_manager_secret_version" "atlas_public_key" {
-  provider = google-beta
-  project  = "ultimateai-terraform-admin"
-  secret   = "atlas_public_key"
-}
--- versions.tf
+++ versions.tf
@@ -1,7 +1,4 @@
 terraform {
 }
 
-provider "mongodbatlas" {
-  public_key  = data.google_secret_manager_secret_version.atlas_public_key.secret_data
-  private_key = data.google_secret_manager_secret_version.atlas_private_key.secret_data
-}
+
