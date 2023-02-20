b0a3ea1fcfe11b568bc1db683787b9b1f996344d
robgutsopedra <roberto.gutierrez@ultimate.ai>
2023-02-20 14:43:53 +0100

    test

--- versions.tf
+++ versions.tf
@@ -1,18 +1,8 @@
 terraform {
-  required_version = ">=1.2.0"
 
   required_providers {
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.45.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.45.0"
-    }
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
-      version = "~> 1.8.0"
     }
   }
 }
