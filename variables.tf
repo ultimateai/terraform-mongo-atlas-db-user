924348fbd512828de3b83d011848840bf88d5afb
Pedro Mealha <pedro.mealha@ultimate.ai>
2024-02-29 11:04:07 +0000

    allow to provide collection name

--- variables.tf
+++ variables.tf
@@ -6,11 +6,11 @@ variable "user_name" {
 variable "databases_roles" {
   description = "List of databases this user needs access to"
   type = list(object({
-    database = string
-    role     = string
+    database   = string
+    collection = optional(string)
+    role       = string
   }))
 }
-
 
 variable "vault_path" {
   description = "Vault's secret path"
@@ -21,6 +21,3 @@ variable "mongo_project_id" {
   description = "Mongo project ID"
   type        = string
 }
-
-
-
